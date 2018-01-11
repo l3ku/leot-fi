5de391fd2537febfe9f7e11bac3da3e42a17aa50
Leo Toikka <leo@seravo.fi>
2018-01-11 17:32:42 +0200

    Fix identation

--- inc/theme-support.php
+++ inc/theme-support.php
@@ -6,13 +6,14 @@
  */
 
 add_theme_support('custom-header', array(
-    'default-image' => get_template_directory_uri() . '/images/apple-camera-desk-office.jpg',
+  'default-image' => get_template_directory_uri() . '/images/apple-camera-desk-office.jpg',
 ));
+
 add_theme_support('custom-logo');
 add_theme_support('post-thumbnails');
 
 function portfolio_register_nav_menus() {
-    register_nav_menu( 'header-menu', __( 'The main navigation menu which appears in the top right corner', 'leotoikka' ) );
+  register_nav_menu( 'header-menu', __( 'The main navigation menu which appears in the top right corner', 'leotoikka' ) );
 }
 add_action( 'after_setup_theme', 'portfolio_register_nav_menus' );
 
